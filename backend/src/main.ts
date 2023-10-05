dda321d98837ab61a46868ea18f583c7ef9af4fc
Vyacheslav <ZhechievViacheslav@gmail.com>
2023-10-05 22:34:51 +0300

    feat: add missing documentation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,3 +1,6 @@
+import * as YAML from 'yaml'
+import { readFileSync } from 'fs';
+import * as swaggerUI from 'swagger-ui-express';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -6,6 +9,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors();
+
+  // TO-DO: Move to dedicated module
+  const swaggerDocs = YAML.parse(readFileSync('documentation/api.yaml', 'utf-8'));
+  const httpAdapter = app.getHttpAdapter();
+  // @ts-ignore
+  httpAdapter.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
   await app.listen(8080);
 }
 
